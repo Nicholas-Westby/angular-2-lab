af59cb998d4f373b13818ae9788fc754640f2444
Nicholas-Westby <nicholas.westby@rhythmagency.com>
2016-11-16 11:23:18 -0800

    Initial commit (plain MVC app that shows a message).

---
+++ AngularLab/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AngularLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = "Testing" as object;
+            return View(model);
+        }
+    }
+}
---
+++ AngularLab/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularLab.Startup))]
+namespace AngularLab
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
